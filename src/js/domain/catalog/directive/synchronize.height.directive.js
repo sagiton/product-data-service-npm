a998f4dacac690eed8ea4f794984261681618ee2
hfudali <hubert.fudali@sagiton.pl>
2017-09-19 17:33:09 +0200

    LSS-25 - Template | Technical data table slider

--- src/js/domain/catalog/directive/synchronize.height.directive.js
+++ src/js/domain/catalog/directive/synchronize.height.directive.js
@@ -3,19 +3,36 @@
         .module('pds.catalog.directive')
         .directive('synchronizeHeight', SynchronizeHeight);
 
-    SynchronizeHeight.$inject = ['$timeout'];
+    SynchronizeHeight.$inject = ['$timeout', '$window'];
 
-    function SynchronizeHeight($timeout) {
+    function SynchronizeHeight($timeout, $window) {
         return {
             restrict: 'EA',
             link: function (scope, element, attrs) {
-                $timeout(function () {
-                    angular
+                $timeout(function() {
+                    var firstColumnRow = angular
                         .element(document)
                         .find('.card.card-column table tr')
                         .eq(scope.$index + 1)
-                        .css('height', element.css('height'));
-                }, 500);
+
+                    scope.$watchCollection(function () {
+                        return {
+                            headerCellHeight: firstColumnRow[0].offsetHeight,
+                            thisCellHeight: element[0].offsetHeight
+                        }
+                    }, synchronizeRowHeight, true)
+
+                    function synchronizeRowHeight(newVals) {
+                        var maxHeight = newVals.headerCellHeight > newVals.thisCellHeight ? newVals.headerCellHeight : newVals.thisCellHeight
+                        element.height(maxHeight)
+                        firstColumnRow.height(maxHeight)
+                    }
+                }, 500)
+
+                angular.element($window).bind('resize', function () {
+                    scope.$apply();
+                });
+
             }
         }
     }
