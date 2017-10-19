8f656a6e02cbebeb5b68872fda9e47c89a7a2b92
hfudali <hubert.fudali@sagiton.pl>
2017-10-19 14:06:09 +0200

    catalog service test added

--- src/js-tests/domain/catalog/service/catalog.service.test.js
+++ src/js-tests/domain/catalog/service/catalog.service.test.js
@@ -1,10 +1,12 @@
 import * as _ from 'lodash'
 import sinon from 'sinon'
-import ResourceMock from "../../../__mocks__/resource.mock";
 
 describe('service: CatalogService', () => {
-    let CatalogService, MenuService, CatalogUrlSchema, catalogSearchListener, Catalog, locale
-    Catalog = ResourceMock({})
+    let CatalogService, MenuService, CatalogUrlSchema, catalogSearchListener, Catalog, locale, CatalogConstructorSpy, $window
+    Catalog = sinon.stub(new class Catalog {
+        $template(){}
+        get(){}
+    }())
 
     beforeEach(angular.mock.module('pds.catalog.service'))
 
@@ -13,40 +15,125 @@ describe('service: CatalogService', () => {
             attr: val => 'OCS_CHANNEL',
             data: () => {}
         });
-        $provide.value('MenuService', MenuService = {})
-        $provide.value('CatalogUrlSchema', CatalogUrlSchema = {})
+        $provide.value('MenuService', MenuService = {
+            findInNavigation: () => {},
+            findParentInNavigation: sinon.stub()
+        })
+        $provide.value('CatalogUrlSchema', CatalogUrlSchema = sinon.stub({
+            build: ()=>{}
+        }))
         $provide.value('catalogSearchListener', catalogSearchListener = {
             listen: () => Promise.resolve({target: {resourceId: 'resourceId'}})
         })
-        $provide.value('Catalog', sinon.stub(Catalog))
+        $provide.value('Catalog', CatalogConstructorSpy = sinon.spy(class {
+            constructor() { return Catalog}
+            static get(){ return {$promise: ''} }
+        }))
         $provide.value('locale', locale = {})
         $provide.value('$q', Promise)
         $provide.value('_', _)
+        $provide.value('$window', $window = sinon.stub({
+            location: {url: ''}
+        }))
     }))
 
     beforeEach(angular.mock.inject(($injector) => {
         CatalogService = $injector.get('CatalogService')
     }));
 
-    it('should calll Catalog with params', async () => {
+    it('should call Catalog with params', async () => {
         await CatalogService.getNewProducts()
-        console.log(Catalog)
+        expect(CatalogConstructorSpy.calledWith({
+            template: {name: 'NEW_PRODUCTS'},
+            model: {
+                locale: '[object Object]',
+                channel: 'OCS_CHANNEL'
+            }
+        })).toBeTruthy()
+        expect(Catalog.$template.called).toBeTruthy()
+    })
 
-        // expect(Catalog.getCal)
-        // spyOn(catalogService, 'getTemplate').and.returnValue(Promise.resolve({
-        //     nodes: [
-        //         {
-        //             "id": 669445,
-        //             "type": "ROOT_CATEGORY",
-        //             "name": "Commercial & Industrial"
-        //         },
-        //         {
-        //             "id": 669446,
-        //             "type": "CATEGORY",
-        //             "name": "Produkte"
-        //         }
-        //     ]
-        // }))
+
+    it('should getById Catalog', async () => {
+        spyOn(MenuService, 'findInNavigation').and.returnValue(Promise.resolve({type: 'catalogType'}))
+        await CatalogService.getCatalogTemplate('catalogId')
+        expect(CatalogConstructorSpy.calledWith({
+            template: {name: 'catalogType'},
+            model: {
+                locale: '[object Object]',
+                channel: 'OCS_CHANNEL',
+                catalogRequest: {
+                    id: 'catalogId',
+                    channel: 'OCS_CHANNEL',
+                    type: 'catalogType'
+                }
+            }
+        })).toBeTruthy()
+        expect(Catalog.$template.called).toBeTruthy()
+    })
+
+    it('should navigateTo', async () => {
+        spyOn(MenuService, 'findInNavigation').and.returnValue(Promise.resolve({type: 'catalogType'}))
+        CatalogConstructorSpy.get = function(){}
+        sinon.stub(CatalogConstructorSpy, 'get').returns({$promise: Promise.resolve({
+            id: 'id',
+            type: 'type',
+            name: 'name'
+        })})
+        spyOn(CatalogUrlSchema, 'build').and.returnValue(Promise.resolve('http://url.url'))
+        await CatalogService.redirectTo('catalogId')
+        expect(CatalogUrlSchema.build.calledWith([{
+            "id": "id",
+            "type": "type",
+            "name": "name"
+        }]))
+        expect($window.location.href).toEqual('http://url.url')
+    })
+
+
+    it('should travelUpNavigationHierarchy', async () => {
+        spyOn(MenuService, 'findInNavigation').and.returnValue(Promise.resolve({
+            id: 'catalogId1',
+            type: 'catalogType1'
+        }))
+        MenuService.findParentInNavigation
+            .onCall(0).returns(Promise.resolve({
+                id: 'catalogId2',
+                type: 'catalogType2'
+            }))
+            .onCall(1).returns(null)
+
+        const tree = await CatalogService.travelUpNavigationHierarchy('catalogId')
+        expect(tree).toMatchSnapshot()
+    })
+
+    it('should getIdFromLocation', () => {
+        const id = CatalogService.getIdFromLocation('https://www.buderus.com/ro/ro/ocs/logamax-plus-gb172i-667905-p/')
+        expect(id).toEqual('667905')
+    })
+
+    it('should resolveUriFromHierarchy', async () => {
+        spyOn(MenuService, 'findInNavigation').and.returnValue(Promise.resolve({
+            id: 'catalogId1',
+            type: 'catalogType1'
+        }))
+        MenuService.findParentInNavigation
+            .onCall(0).returns(Promise.resolve({
+                id: 'catalogId2',
+                type: 'catalogType2'
+            }))
+            .onCall(1).returns(null)
+        await CatalogService.resolveUriFromHierarchy('categoryId')
+        expect(CatalogUrlSchema.build.calledWith([
+            {
+                "id": "catalogId1",
+                "type": "catalogType1"
+            },
+            {
+                "id": "catalogId2",
+                "type": "catalogType2"
+            }
+        ])).toBeTruthy()
     })
 
 })
