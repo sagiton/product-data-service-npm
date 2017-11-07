6312968c9bfae921f0669a23f5f1af79b65495a4
hfudali <hubert.fudali@sagiton.pl>
2017-11-07 12:26:04 +0100

    ocs-search fix

--- src/js/common/service/spinner.service.js
+++ src/js/common/service/spinner.service.js
@@ -8,8 +8,20 @@
     function SpinnerService($http) {
         var self = this;
 
-        self.isLoading = function () {
-            return $http.pendingRequests.length > 0;
+        self.isLoading = isLoading
+        self.startLoading = startLoading
+        self.stopLoading = stopLoading
+
+        function isLoading() {
+            return $http.pendingRequests.length > 0 || self.loading;
+        }
+
+        function startLoading() {
+            self.loading = true
+        }
+
+        function stopLoading() {
+            self.loading = false
         }
 
     }
