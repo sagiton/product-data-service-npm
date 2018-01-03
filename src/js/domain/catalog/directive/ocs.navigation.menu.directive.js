d0769af31677bbae6d5143320b1dba82359592b9
Mateusz Rutski <mateusz.rutski@sagiton.pl>
2018-01-03 15:53:48 +0100

    OCS Navigation custom template

--- src/js/domain/catalog/directive/ocs.navigation.menu.directive.js
+++ src/js/domain/catalog/directive/ocs.navigation.menu.directive.js
@@ -3,31 +3,26 @@
         .module('pds.catalog.directive')
         .directive('ocsNavigationMenu', OcsNavigationMenu);
 
-    OcsNavigationMenu.$inject = ['config', '$templateCache']
+    OcsNavigationMenu.$inject = []
 
-    function OcsNavigationMenu(config, $templateCache) {
+    function OcsNavigationMenu() {
         return {
             restrict: 'A',
             scope: true,
             transclude: true,
-            template: function(element, attrs) {
-                return $templateCache.get('component/navigation_menu.html')
-            },
             controller: NavigationMenuController,
             controllerAs: '$ctrl'
         }
     }
 
-    NavigationMenuController.$inject = ['$scope', 'MenuService', '$element', '$transclude']
+    NavigationMenuController.$inject = ['$scope', 'MenuService', '$element', '$transclude', '$templateCache', '$compile']
 
-    function NavigationMenuController($scope, MenuService, $element, $transclude) {
+    function NavigationMenuController($scope, MenuService, $element, $transclude, $templateCache, $compile) {
         var self = this;
-        $scope.abc = {a: 1};
         $transclude($scope, function (clone) {
             self.transcluded = !!clone.length;
-            if (self.transcluded) {
-                $element.replaceWith(clone);
-            }
+            $element.empty();
+            $element.append(self.transcluded ? clone : $compile($templateCache.get('component/navigation_menu.html'))($scope));
         });
         self.menu = self.menu || {
             name: $element.children('a').text()
