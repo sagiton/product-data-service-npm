b52da8c76da4d94e89b584b956183250a088bb6c
Mateusz Rutski <mateusz.rutski@sagiton.pl>
2018-01-03 15:13:41 +0100

    OCS Navigation custom template

--- src/js/domain/catalog/directive/ocs.navigation.menu.directive.js
+++ src/js/domain/catalog/directive/ocs.navigation.menu.directive.js
@@ -9,7 +9,8 @@
         return {
             restrict: 'A',
             scope: true,
-            template:  function() {
+            transclude: true,
+            template: function(element, attrs) {
                 return $templateCache.get('component/navigation_menu.html')
             },
             controller: NavigationMenuController,
@@ -17,11 +18,17 @@
         }
     }
 
-    NavigationMenuController.$inject = ['MenuService', '$element']
+    NavigationMenuController.$inject = ['$scope', 'MenuService', '$element', '$transclude']
 
-    function NavigationMenuController(MenuService, $element) {
+    function NavigationMenuController($scope, MenuService, $element, $transclude) {
         var self = this;
-
+        $scope.abc = {a: 1};
+        $transclude($scope, function (clone) {
+            self.transcluded = !!clone.length;
+            if (self.transcluded) {
+                $element.replaceWith(clone);
+            }
+        });
         self.menu = self.menu || {
             name: $element.children('a').text()
         };
