ce9cd39f80f59b162e9c28769b083464b3cef22c
Mateusz Rutski <mateusz.rutski@sagiton.pl>
2018-03-21 13:07:35 +0100

    Search default image get from translations

--- src/js/common/directive/httpSrc.directive.js
+++ src/js/common/directive/httpSrc.directive.js
@@ -3,19 +3,11 @@
 
     angular
         .module('pds.common.directive')
-        .directive('httpSrc', HttpSrc)
+        .directive('httpSrc', HttpSrc);
 
-    var defaultImages = {
-        'img-bg_BG': '/src/media/img_Default_BG_1.jpg',
-        'img-es_ES': '/src/media/img_Default_ES_1.jpg',
-        'img-hu_HU': '/src/media/img_Default_HU_1.jpg',
-        'img-ro_RO': '/src/media/img_Default_RO_1.jpg'
-    };
-    var defaultLang = 'bg_BG'
+    HttpSrc.$inject = ['$http', 'translateFilter'];
 
-    HttpSrc.$inject = ['_', '$http', 'locale'];
-
-    function HttpSrc(_, $http, locale) {
+    function HttpSrc($http, translateFilter) {
         return {
             scope: {
                 httpSrc: '='
@@ -39,8 +31,7 @@
             element.bind('error', defaultImage);
 
             function defaultImage() {
-                var image = defaultImages['img-'+locale.toString()] || defaultImages['img-'+defaultLang]
-                attrs.$set('src', image);
+                attrs.$set('src', translateFilter('search.default.image.path'));
             }
 
         }
