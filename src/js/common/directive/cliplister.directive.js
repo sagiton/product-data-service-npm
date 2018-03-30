5471eeeb32260266cbcffc2eb657014fe521d0e2
Mateusz Rutski <mateusz.rutski@sagiton.pl>
2018-03-30 14:52:54 +0200

    BOTTWEB-412 Image slider videos

--- src/js/common/directive/cliplister.directive.js
+++ src/js/common/directive/cliplister.directive.js
@@ -3,48 +3,53 @@
         .module('pds.common.directive')
         .directive('cliplister', CliplisterDirective);
 
-    CliplisterDirective.$inject = ['$filter', '$sce'];
+    CliplisterDirective.$inject = ['locale', '$timeout'];
 
-    function CliplisterDirective($filter, $sce) {
+    function CliplisterDirective(locale, $timeout) {
         return {
             restrict: 'E',
-            template: '<div id="video" style="height:400px;"></div>',
+            template: '<div class="video-player" style="height:400px;"></div>',
             scope: {
                 videoId: '=videoId'
             },
             link: function(scope, element, attrs) {
-                new CliplisterControl.Viewer({
-                    parentId: "video",
-                    customer: 157893,
-                    assets: [scope.videoId],
-                    keyType: 10000,
-                    fsk: 18,
-                    autoplay: false,
-                    plugins: {
-                        InnerControls: {
-                            layer: 2,
-                            mobileDefaultControls: true,
-                            id: "controls",
-                            blacklist: ["share","quality","playback-speed"],
-                            template: {
-                                type: "external",
-                                source: "https://mycliplister.com/static/viewer/assets/skins/default/controls.html"
-                            }
-                        },
-                        ClickableVideo: {layer: 1},
-                        PlayButton: {
-                            id: "playButton",
-                            layer: 7,
-                            image: "https://mycliplister.com/static/viewer/assets/skins/default/playButton.png",
-                            width: 100,
-                            height: 100
-                        },
-                        PreviewImage: {layer: 6}
-                    }
-                });
+                var id = 'video' + Math.floor(Math.random()*1000);
+                element.attr('id', id);
+                $timeout(function () {
+                    new CliplisterControl.Viewer({
+                        parentId: id,
+                        customer: 157893,
+                        assets: [scope.videoId],
+                        keyType: 10000,
+                        lang: locale.language,
+                        fsk: 18,
+                        autoplay: false,
+                        plugins: {
+                            InnerControls: {
+                                layer: 2,
+                                mobileDefaultControls: true,
+                                id: "controls",
+                                blacklist: ["share","quality","playback-speed"],
+                                template: {
+                                    type: "external",
+                                    source: "https://mycliplister.com/static/viewer/assets/skins/default/controls.html"
+                                }
+                            },
+                            ClickableVideo: {layer: 1},
+                            PlayButton: {
+                                id: "playButton",
+                                layer: 7,
+                                image: "https://mycliplister.com/static/viewer/assets/skins/default/playButton.png",
+                                width: 100,
+                                height: 100
+                            },
+                            PreviewImage: {layer: 6}
+                        }
+                    });
+                }, 500);
             },
             replace: true
         }
-    };
+    }
 
 })(angular);
