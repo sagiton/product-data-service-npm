8f7e86a7eecdb9e6cc72cd0d83ef5327dbee6adb
Mateusz Rutski <mateusz.rutski@sagiton.pl>
2018-04-25 15:20:25 +0200

    BOTTWEB-456 Embedded template compile fix

--- src/js/domain/catalog/catalog.module.js
+++ src/js/domain/catalog/catalog.module.js
@@ -2,7 +2,7 @@
     angular.module('pds.catalog.route', ['pds.common.route', 'ui.router', 'ncy-angular-breadcrumb']);
     angular.module('pds.catalog.service', ['pds.common.filter', 'pds.common.config']);
     angular.module('pds.catalog.config', ['pds.environment', 'ngResource', 'pds.common.config']);
-    angular.module('pds.catalog.controller', ['ngSanitize', 'datatables', 'hl.sticky', 'dcbImgFallback', 'slickCarousel', 'pds.catalog.service', 'pds.catalog.directive', 'pds.navigation.service']);
+    angular.module('pds.catalog.controller', ['ngSanitize', 'datatables', 'hl.sticky', 'dcbImgFallback', 'slickCarousel', 'pds.catalog.service', 'pds.catalog.directive', 'pds.navigation.service', 'angular-bind-html-compile']);
     angular.module('pds.catalog.model', []);
     angular.module('pds.catalog.factory', ['pds.catalog.service']);
     angular.module('pds.catalog.directive', ['pds.common.factory']);
